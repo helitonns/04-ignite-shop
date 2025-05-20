238eaa218d4f47f67e096f1f5147d467050eee53
helitonns <helitonns@gmail.com>
2025-05-20 16:40:24 -0400

    Imagens no Next.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import Image from "next/image";
 import logoImg from "../assets/logo.svg";
 import { globalStyles } from "../styles/global";
 import { Container, Header } from "../styles/pages/app";
@@ -9,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return(
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
 
       <Component {...pageProps} />
