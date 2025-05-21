cf76c1b35acda98acfa54f16bf3257ed343396d8
helitonns <helitonns@gmail.com>
2025-05-21 17:39:14 -0400

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,21 @@
 import { useRouter } from "next/router";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/products";
 
 export default function Product() {
-  const { query} = useRouter();
+  const { query } = useRouter();
   
   return (
-    <h1>Produto {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 67,90</span>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum veniam consequuntur quis nisi ratione voluptatem repellendus obcaecati minima neque, quos soluta, molestiae eos ea officiis esse dolorum. Tempora, sint placeat.</p>
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
