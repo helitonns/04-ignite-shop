cf41c7d87b7bff0d1020fbd96ddd588157862c89
helitonns <helitonns@gmail.com>
2025-05-22 19:57:56 -0400

    Estrutura de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Product() {
   return (
-    <h1>Sucesso!</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhul! <strong>Heliton</strong>, sua <strong>Camiseta Beyond</strong> já
+        está a caminho da sua casa.
+      </p>
+
+      <Link href="/">
+        Voltar ao catálago
+      </Link>
+    </SuccessContainer>
   );
 }
