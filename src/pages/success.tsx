b014a8d283762e6d12b9325ff984b9b426362796
Heliton <49277165+helitonns@users.noreply.github.com>
2025-05-23 13:59:12 +0000

    Dados da compra no sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,20 +1,60 @@
 import Link from "next/link";
 import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+import { GetServerSideProps } from "next";
+import { stripe } from "../lib/stripe";
+import Stripe from "stripe";
+import Image from "next/image";
 
-export default function Product() {
+interface SuccessProps {
+  customerName: string;
+  product: {
+    name: string;
+    imageUrl: string;
+  };
+}
+
+export default function Product({ customerName, product }: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra efetuada!</h1>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt="" />
+      </ImageContainer>
 
       <p>
-        Uhul! <strong>Heliton</strong>, sua <strong>Camiseta Beyond</strong> já
-        está a caminho da sua casa.
+        Uhul! <strong>{customerName}</strong>, sua{" "}
+        <strong>{product.name}</strong> já está a caminho da sua casa.
       </p>
 
-      <Link href="/">
-        Voltar ao catálago
-      </Link>
+      <Link href="/">Voltar ao catálago</Link>
     </SuccessContainer>
   );
 }
+
+//Formas de fetch de dados, 3 formas:
+//1º - Client-side {useEffect} com axios -> 1° pensar numa tela d loading; 2º a api do
+//stripe não permite buscar os dados de uma session por uma chamada client side,
+//não seria seguro, pois exporia a chave secreta
+//2º - getStaticProps -> não faz sentindo, porque a página sempre trará uma informação dinâmica
+//3º - getServerSideProps -> forma escolhida
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ["line_items", "line_items.data.price.product"],
+  });
+
+  const customerName = session.customer_details.name;
+  const product = session.line_items.data[0].price.product as Stripe.Product;
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  };
+};
